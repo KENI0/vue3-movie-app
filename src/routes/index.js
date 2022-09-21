8e67c6f2e9803dc72783b1971d525d35dc854169
KENI0 <chl0933@naver.com>
2022-09-21 22:29:23 +0900

    로고 생성

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 import Home from './Home'
 import About from './About'
+import Movie from './Movie'
 
 
 export default createRouter({
@@ -14,5 +15,9 @@ export default createRouter({
       path: "/about",
       component: About,
     },
+    {
+      path: "/Movie",
+      component: Movie
+    }
   ],
 });
